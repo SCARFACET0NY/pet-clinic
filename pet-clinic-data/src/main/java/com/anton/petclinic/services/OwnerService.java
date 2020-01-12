04da856d7d7a24f5cc532ddc457d31f22a587309
Anton <ssa.ssa.ssa11@gmail.com>
2020-01-12 17:02:53 +0200

    Refactored to common crud interface.

--- pet-clinic-data/src/main/java/com/anton/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/anton/petclinic/services/OwnerService.java
@@ -2,14 +2,6 @@ package com.anton.petclinic.services;
 
 import com.anton.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName();
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
