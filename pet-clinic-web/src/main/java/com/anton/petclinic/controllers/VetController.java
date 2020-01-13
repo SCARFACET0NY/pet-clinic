7150e96d35c0eb2d8ae149f7a997d3d39f9eeca5
Anton <ssa.ssa.ssa11@gmail.com>
2020-01-13 14:24:45 +0200

    added Owners List and updated controller.

--- pet-clinic-web/src/main/java/com/anton/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/anton/petclinic/controllers/VetController.java
@@ -1,10 +1,17 @@
 package com.anton.petclinic.controllers;
 
+import com.anton.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("/vets")
     public String listVets() {
         return "vets/index";
