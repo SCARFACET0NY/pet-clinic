085ce3c0af005e0a5e7994fce763ad40108810ec
Anton <ssa.ssa.ssa11@gmail.com>
2020-01-13 14:57:41 +0200

    added Vets List and updated controller.

--- pet-clinic-web/src/main/java/com/anton/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/anton/petclinic/bootstrap/DataLoader.java
@@ -20,13 +20,11 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Name 1");
         owner1.setLastName("Surname 1");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Name 2");
         owner2.setLastName("Surname 2");
         ownerService.save(owner2);
@@ -34,13 +32,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Name 1");
         vet1.setLastName("Surname 1");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Name 2");
         vet2.setLastName("Surname 2");
         vetService.save(vet2);
