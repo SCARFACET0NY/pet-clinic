3ab16ce2cd42eca9f484634144a18dd84cd57091
Anton <ssa.ssa.ssa11@gmail.com>
2020-01-21 21:14:15 +0200

    Added map service for visit.

---
+++ pet-clinic-data/src/main/java/com/anton/petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.anton.petclinic.services;
+
+import com.anton.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/anton/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package com.anton.petclinic.services.map;
+
+import com.anton.petclinic.model.Visit;
+import com.anton.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+            || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
